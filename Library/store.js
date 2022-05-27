50a1ec3512042e4dadb475cda690d2921620fb45
Janani Raguram <janani689@gmail.com>
2022-05-26 21:29:22 -0400

    added skeleton of book info input

--- Library/store.js
+++ Library/store.js
@@ -1,3 +1,5 @@
+let mylibrary = [];
+
 function Book(title, author, numPgs, haveRead){
     this.title = title;
     this.author = author;
@@ -13,4 +15,30 @@ Book.prototype.info = function(){
         return(title + " by " + author + ", " + numPgs + " pages, already read.");
 
     }
+}
+
+function addToLibrary(title, author, numPgs, haveRead){
+    let newBook = new Book(title, author, numPgs, haveRead);
+    mylibrary.push(newBook);
+}
+
+function inLibrary(){
+    let i = 0;
+    while(mylibrary.length != 0){
+        mylibrary[i].info;
+        i++;
+    }
+}
+
+function showForm(){
+    document.getElementById("form").style.display = "block";
+}
+
+
+function inputBookInfo(){
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let numPgs = document.getElementById("numPgs").value
+    let hasRead = document.getElementById("hasRead").checked;
+    addToLibrary(title, author, numPgs, hasRead);
 }
