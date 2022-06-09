de85a9b628c31e2e48c52e7d3057504e0c61b401
jraguram <janani689@gmail.com>
2022-06-08 21:31:41 -0400

    changed table layout of info to text

--- Library/store.js
+++ Library/store.js
@@ -1,54 +1,64 @@
-let mylibrary = [];
+$(document).ready(function(){
+    let mylibrary = [];
 
-function Book(title, author, numPgs, haveRead){
-    this.title = title;
-    this.author = author;
-    this.numPgs = numPgs;
-    this.haveRead = haveRead;
-}
-
-Book.prototype.info = function(){
+    function Book(title, author, numPgs, haveRead){
+        this.title = title;
+        this.author = author;
+        this.numPgs = numPgs;
+        this.haveRead = haveRead;
+    }
+    
+    Book.prototype.info = function(){
+            
+        if(this.haveRead == false){
+            return(this.title + "By " + this.author + "\nPgs" + this.numPgs + "\nnot read yet.");
+        }else{
+            return(this.title + "\nBy " + this.author + "\nPgs" + this.numPgs + "\nalready read.");
+    
+        }
+    }
+    
+    function addToLibrary(title, author, numPgs, haveRead){
+        let newBook = new Book(title, author, numPgs, haveRead);
+        mylibrary.push(newBook);
+        // console.log(newBook.info());
+    
+        //show in browser
         
-    if(this.haveRead == false){
-        return(this.title + "By " + this.author + "\nPgs" + this.numPgs + "\nnot read yet.");
-    }else{
-        return(this.title + "\nBy " + this.author + "\nPgs" + this.numPgs + "\nalready read.");
-
+        let i = 0;
+        for(bookInfo in newBook){
+            // console.log(bookInfo, newBook[bookInfo]);
+            let book = document.createElement("div");
+            let bookDesc = document.createElement("p");
+            let text = document.createTextNode( newBook[bookInfo]);
+            book.appendChild(bookDesc);
+            bookDesc.appendChild(text);
+            let myBookshelf = document.getElementById("myLibrary");
+            myBookshelf.appendChild(book);
+            
+            if(i == 3){
+                break;
+            }
+            i++;
+        }
+      
     }
-}
-
-function addToLibrary(title, author, numPgs, haveRead){
-    let newBook = new Book(title, author, numPgs, haveRead);
-    mylibrary.push(newBook);
-    console.log(newBook.info());
-
-    //show in browser
-    let bookshelf = document.createElement("p");
-    let bookDesc = document.createTextNode(newBook.info());
-    bookshelf.appendChild(bookDesc);
-    let myBookshelf = document.getElementById("myLibrary");
-    myBookshelf.appendChild(bookshelf);
-}
-
-function inLibrary(){
-    let i = 0;
-    while(mylibrary.length != 0){
-        mylibrary[i].info;
-        i++;
-    }
-}
-
-function showForm(){
-    document.getElementById("form").style.display = "block";
-}
-
-
-function inputBookInfo(){
-    let title = document.getElementById("title").value;
-    let author = document.getElementById("author").value;
-    let numPgs = document.getElementById("numPgs").value
-    let hasRead = document.getElementById("hasRead").checked;
-    addToLibrary(title, author, numPgs, hasRead);
-
-}
+    
+    
+    $('#addInfo').on('click',function (e){
+        document.getElementById("form").style.display = "block";
+    });
+    
+    $('#inputBookInfo').on('click',function (e){
+        let title = document.getElementById("title").value;
+        let author = document.getElementById("author").value;
+        let numPgs = document.getElementById("numPgs").value
+        let hasRead = document.getElementById("hasRead").checked;
+        addToLibrary(title, author, numPgs, hasRead);
+    
+    });
+    
+    
+    
+});
 
