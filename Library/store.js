0d80fcd7903c789b2b399d14617bbd02f1cc0248
jraguram <janani689@gmail.com>
2022-07-09 13:42:04 -0400

    added cells for books

--- Library/store.js
+++ Library/store.js
@@ -17,45 +17,72 @@ $(document).ready(function(){
     
         }
     }
-    
-    function addToLibrary(title, author, numPgs, haveRead){
-        let newBook = new Book(title, author, numPgs, haveRead);
+
+    function addToLibrary(title, author, numPgs, hasRead){
+        let newBook = new Book(title, author, numPgs, hasRead);
         mylibrary.push(newBook);
         // console.log(newBook.info());
-    
-        //show in browser
-        
-        let i = 0;
-        for(bookInfo in newBook){
-            // console.log(bookInfo, newBook[bookInfo]);
-            let book = document.createElement("div");
-            let bookDesc = document.createElement("p");
-            let text = document.createTextNode( newBook[bookInfo]);
-            book.appendChild(bookDesc);
-            bookDesc.appendChild(text);
-            let myBookshelf = document.getElementById("myLibrary");
-            myBookshelf.appendChild(book);
-            
-            if(i == 3){
-                break;
-            }
-            i++;
+
+
+        let table = document.getElementById('table');
+        let newRow, newCell; 
+
+        //with added book library size is 1 or 1 more than 4 (need a new row)
+        if(mylibrary.length == 1 || mylibrary.length % 4 == 1){
+            newRow = table.insertRow();
         }
-      
+
+        let rowNum = table.rows.length-1;
+        
+        newCell = table.rows[rowNum].insertCell();
+        
+        //put input details together 
+        let bookDetails = document.createElement('ul');
+        let details = document.createElement('li');
+
+        //title
+        details.textContent = title 
+        bookDetails.appendChild(details);
+
+        //author
+        details = document.createElement('li');
+        details.textContent = author;
+        bookDetails.appendChild(details);
+
+        //numpg
+        details = document.createElement('li');
+        details.textContent = numPgs;
+        bookDetails.appendChild(details);
+
+        //has read or not
+        details = document.createElement('li');
+        details.textContent = hasRead;
+        bookDetails.appendChild(details);
+
+
+        //details show in browser
+        newCell.appendChild(bookDetails);
+
     }
     
     
-    $('#addInfo').on('click',function (e){
-        document.getElementById("form").style.display = "block";
-    });
-    
     $('#inputBookInfo').on('click',function (e){
-        let title = document.getElementById("title").value;
-        let author = document.getElementById("author").value;
-        let numPgs = document.getElementById("numPgs").value
-        let hasRead = document.getElementById("hasRead").checked;
+        // let title = document.getElementById("title").value;
+        // let author = document.getElementById("author").value;
+        // let numPgs = document.getElementById("numPgs").value
+        // let hasRead = document.getElementById("hasRead").checked;
+        let title = "rt";
+        let author = "rt";
+        let numPgs = "90";
+        let hasRead = true;
+        if(hasRead == true){
+            hasRead = "Already Read";
+        }else{
+            hasRead = "TBR";
+        }
+        console.log(mylibrary);
         addToLibrary(title, author, numPgs, hasRead);
-        document.getElementById("form").style.display = "none";
+        // document.getElementById("form").style.visibility = "hidden";
     });
     
     
